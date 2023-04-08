b4a104e68889fd00be731742c334f1fb1137dba3
Harshal <harshal.shirke@iauro.com>
2023-04-08 17:34:56 +0530

    Harshal :- NBA tracker application build

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const ENVIRONMENT = {
-	headers: {
-		'X-RapidAPI-Key': '2QMXSehDLSmshDmRQcKUIAiQjIZAp1UvKUrjsnewgqSP6F5oBX',
-		'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
-	},
-	baseUrl: 'https://free-nba.p.rapidapi.com',
-	logoUrl: 'https://interstate21.com/nba-logos/'
-};
